01d31f916ae9afef29ec60f61f63d1ba129ec2a5
unknown <sahribesh0123@gmail.com>
2024-05-09 17:04:23 +0545

    hard coded

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'MyLiveTV',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
@@ -46,64 +46,71 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomSheet: Text('By @rkraaz' ,style: TextStyle(fontSize: 15 , fontWeight: FontWeight.bold),),
       appBar: AppBar(
+        backgroundColor: Color(0xFF8A55DF),
+        // backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Container(
-          alignment: Alignment.center,
-          color: Color.fromRGBO(89, 128, 244, 1),
-          height: 250,
-          width: double.infinity,
-          child: 
-          Text('MyLiveTv')),
+            alignment: Alignment.center,
+            child: Text(
+              'MyLiveTv',
+              style: TextStyle(
+                  color: Color(0xFFF9F8F9),
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            )),
       ),
-      body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Column(
-              children: [
-                SizedBox(
-                  height: 50,
-                ),
-                IconButton(
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => cricket()));
-                    // Navigator.push(context,
-                    //     MaterialPageRoute(builder: (context) => VideoApp()));
-
-                  },
-                  icon: const Icon(Icons.sports_cricket_rounded),
-                  iconSize: 50,
-                ),
-                const Text('Cricket Channels'),
-                SizedBox(
-                  height: 50,
-                ),
-                IconButton(
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => Music()));
-                  },
-                  icon: const Icon(Icons.music_note_rounded),
-                  iconSize: 50,
-                ),
-                const Text('Music Channels'),
-                SizedBox(
-                  height: 50,
-                ),
-                IconButton(
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => Cartoon()));
-                  },
-                  icon: const Icon(Icons.bedroom_baby_outlined),
-                  iconSize: 50,
-                ),
-                const Text('Cartoon Channels'),
-              ],
-            ),
-          ],
+      body: Container(
+        child: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Column(
+                children: [
+                  SizedBox(
+                    height: 50,
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => cricket()));
+                      // Navigator.push(context,
+                      //     MaterialPageRoute(builder: (context) => VideoApp()));
+                    },
+                    icon: const Icon(Icons.sports_cricket_rounded),
+                    iconSize: 50,
+                  ),
+                  const Text('Cricket Channels'),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => Music()));
+                    },
+                    icon: const Icon(Icons.music_note_rounded),
+                    iconSize: 50,
+                  ),
+                  const Text('Music Channels'),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => Cartoon()));
+                    },
+                    icon: const Icon(Icons.bedroom_baby_outlined),
+                    iconSize: 50,
+                  ),
+                  const Text('Cartoon Channels'),
+                ],
+              ),
+            ],
+          ),
         ),
+        
       ),
       // This trailing comma makes auto-formatting nicer for build methods.
     );
